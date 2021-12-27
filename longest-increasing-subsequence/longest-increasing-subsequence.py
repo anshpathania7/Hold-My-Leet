78a910951f1a1681f9fb86eeeb74904a34cc4c36
anshpathania7 <55742309+anshpathania7@users.noreply.github.com>
2021-12-27 13:55:30 +0530

    Time: 60 ms (98.78%), Space: 14.5 MB (76.16%) - LeetHub

---
+++ longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -0,0 +1,18 @@
+from bisect import bisect_left
+
+class Solution:
+    def getIndexOfNextGreaterElement(self, n, dp)->int:
+        pos = bisect_left(dp,n)
+        if pos==len(dp):
+            return None
+        else:
+            return pos
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp=[]
+        for i in range(len(nums)):
+            index = self.getIndexOfNextGreaterElement(nums[i],dp)
+            if index is None:
+                dp.append(nums[i])
+            else:
+                dp[index] = nums[i]
+        return len(dp)
