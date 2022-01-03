4ae1ef4eef49a4c3501ff1739b0ac393b9b98188
anshpathania7 <55742309+anshpathania7@users.noreply.github.com>
2022-01-04 00:53:33 +0530

    Time: 32 ms (78.65%), Space: 14.4 MB (13.26%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -3,6 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        nums.sort()       
+        """
         count=0
         
         #First, put all 0s to start
@@ -10,8 +12,10 @@ class Solution:
             if nums[i]==0:
                 nums[count],nums[i] = 0, nums[count]
                 count+=1
+        #Now Putting all 1st after 0s
         for i in range(len(nums)):
             if nums[i]==1:
                 nums[count],nums[i] = 1, nums[count]
                 count+=1
+        """
         
